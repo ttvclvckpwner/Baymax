3dc8f386b413d58a4a243e0206c2f95c44d0a09b
CARL CLACK <350478@guhsd.net>
2020-10-07 19:37:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+#greeting
+name = input("Enter your name:")
+age = input("Enter your age:")
+
+print("Hello, " + name + " you are " + age + " years old!" + correct?)
+answer = input("Correct or No?:")
+correct = True
+no = False
+if 'correct':
+    print(correct)
+elif 'no': 
+    print(no)
+
+#Baymax introduces himself
+print( " My name is Baymax!")
+
+num1 = input("Enter a number:")
+num2 = input("Enter another number:")
+result = float(num1) + float(num2)
+print(result)
